24f320ed73f016f589ac4657cc54ef45ea9e5fc1
genaroibc <bonavitaismael@gmail.com>
2023-01-25 17:57:06 -0300

    [FEAT]: update main Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import { Header } from "./components/Header/Header";
+import "./globals.css";
 
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
-  )
+  );
 }
