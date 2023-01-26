4226ba399d8ec31f01f2ef8cf031fcaf1e1cccdb
genaroibc <bonavitaismael@gmail.com>
2023-01-25 23:58:03 -0300

    [DEV]: add Generations component

---
+++ src/app/components/BlogGenerator/components/Generations/Generations.tsx
@@ -0,0 +1,25 @@
+import { GENERATION_PROMPT_PREFIX } from "@/services/constants";
+import { cohereResponse, generateResponse } from "cohere-ai/dist/models";
+import styles from "./Generations.module.css";
+
+type Props = {
+  generations: cohereResponse<generateResponse>["body"]["generations"];
+  prompt: string;
+};
+
+export function Generations({ generations, prompt }: Props) {
+  return (
+    <div className={styles.generations}>
+      {generations.map(({ text }, i) => (
+        <article key={i} className={styles.generations__item}>
+          <h4 className={styles.generations__item__title}>
+            {prompt
+              .replace(`${GENERATION_PROMPT_PREFIX}`, "")
+              .replaceAll(".", "")}
+          </h4>
+          <p className={styles.generations__item__content}>{text.trim()}...</p>
+        </article>
+      ))}
+    </div>
+  );
+}
