a115b2c8c35317de993a5d272aab5a8316e49849
genaroibc <bonavitaismael@gmail.com>
2023-01-26 16:47:51 -0300

    [FEAT]: update Generations component

--- src/app/components/BlogGenerator/components/Generations/Generations.tsx
+++ src/app/components/BlogGenerator/components/Generations/Generations.tsx
@@ -1,6 +1,7 @@
 import { GENERATION_PROMPT_PREFIX } from "@/constants";
 import { cohereResponse, generateResponse } from "cohere-ai/dist/models";
 import styles from "./Generations.module.css";
+import { GenerationSlice } from "./GenerationSlice/GenerationSlice";
 
 type Props = {
   generations: cohereResponse<generateResponse>["body"]["generations"];
@@ -12,12 +13,25 @@ export function Generations({ generations, prompt }: Props) {
     <div className={styles.generations}>
       {generations.map(({ text }, i) => (
         <article key={i} className={styles.generations__item}>
-          <h4 className={styles.generations__item__title}>
-            {prompt
-              .replace(`${GENERATION_PROMPT_PREFIX}`, "")
-              .replaceAll(".", "")}
-          </h4>
-          <p className={styles.generations__item__content}>{text.trim()}...</p>
+          <>
+            <h4 className={styles.generations__item__title}>
+              {prompt
+                .trim()
+                .replace(`${GENERATION_PROMPT_PREFIX}`, "")
+                .replaceAll(".", "")}
+            </h4>
+
+            {text
+              .trim()
+              .split("\n\n")
+              .map((slice, i) => (
+                <GenerationSlice
+                  handleDeleteSlice={() => {}}
+                  key={i}
+                  initialContent={slice.trim()}
+                />
+              ))}
+          </>
         </article>
       ))}
     </div>
