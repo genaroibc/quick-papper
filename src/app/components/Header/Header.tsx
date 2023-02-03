5adc569a777e517884ccbadda9a1f43cbee247c6
genaroibc <bonavitaismael@gmail.com>
2023-02-03 08:37:28 -0300

    [FEAT]: style Header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,6 +4,9 @@ import styles from "./Header.module.css";
 export function Header() {
   return (
     <header className={styles.header}>
+      <div className={styles.header__logo}>
+        <span>Quick</span> <span>Papper</span>
+      </div>
       <nav className={styles.header__nav}>
         <Link className={styles.header__nav__link} href="/">
           Home
