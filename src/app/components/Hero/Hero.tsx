d4a2e431e91f793e8f6019d915b59782bfeaf7e4
genaroibc <bonavitaismael@gmail.com>
2023-02-03 14:35:56 -0300

    [FEAT]: style Hero section

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,31 +1,39 @@
 import { GradientTitle } from "../GradientTitle/GradientTitle";
+import { Model3d } from "../Model3d/Model3d";
 import styles from "./Hero.module.css";
 
 export function Hero() {
   return (
     <section className={styles.hero}>
-      <h1 className={styles.hero__title}>Quick Papper</h1>
-      <h2 className={styles.hero__subtitle}>
-        Create{" "}
-        <GradientTitle
-          colors={["var(--terciary-color)", "var(--secondary-color)"]}
-        >
-          high-quality
-        </GradientTitle>{" "}
-        articles{" "}
-        <GradientTitle
-          colors={["var(--terciary-color)", "var(--secondary-color)"]}
-        >
-          faster
-        </GradientTitle>{" "}
-        than ever
-      </h2>
+      <div>
+        <h1 className={styles.hero__title}>
+          Quick <span>Papper</span>
+        </h1>
+        <h2 className={styles.hero__subtitle}>
+          Create{" "}
+          <GradientTitle
+            colors={["var(--terciary-color)", "var(--complement-color)"]}
+          >
+            high-quality
+          </GradientTitle>{" "}
+          articles{" "}
+          <GradientTitle
+            colors={["var(--terciary-color)", "var(--complement-color)"]}
+          >
+            faster
+          </GradientTitle>{" "}
+          than ever
+        </h2>
+        <p>✋ Stop wasting time!</p>
+        <p>
+          Generate the AI-powered draft that you need to get inspired and take
+          your productivity to the moon 🚀
+        </p>
+      </div>
 
-      <p>✋ Stop wasting time!</p>
-      <p>
-        Generate the AI-powered draft that you need to get inspired and take
-        your productivity to the moon 🚀
-      </p>
+      <div className={styles.hero__image}>
+        <Model3d />
+      </div>
     </section>
   );
 }
