da02ce2989b7481dd5669173bf549cfa85319ae5
genaroibc <bonavitaismael@gmail.com>
2023-02-06 17:11:49 -0300

    [FEAT]: fire confetti when copying result

--- src/app/components/Generation/CopyNavBar/CopyNavBar.tsx
+++ src/app/components/Generation/CopyNavBar/CopyNavBar.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useRef } from "react";
+import ReactCanvasConfetti from "react-canvas-confetti";
 import { formatToHTML } from "@/utils/formatToHTML/formatToHTML";
 import Image from "next/image";
 import styles from "./CopyNavBar.module.css";
@@ -19,7 +21,56 @@ export function CopyNavBar({ text, title }: Props) {
     };
 
     navigator.clipboard.writeText(FORMAT_DICT[format](title, text));
+    fireConfetti();
   };
+
+  const refAnimationInstance = useRef(null);
+
+  const getInstance = useCallback((instance: any) => {
+    refAnimationInstance.current = instance;
+  }, []);
+
+  const makeConfettiShot = useCallback((particleRatio: number, opts: {}) => {
+    const animationInstace = refAnimationInstance.current as unknown as (
+      opts: any
+    ) => void;
+
+    refAnimationInstance.current &&
+      animationInstace({
+        ...opts,
+        origin: { y: 0.7 },
+        particleCount: Math.floor(200 * particleRatio)
+      });
+  }, []);
+
+  const fireConfetti = useCallback(() => {
+    makeConfettiShot(0.25, {
+      spread: 26,
+      startVelocity: 55
+    });
+
+    makeConfettiShot(0.2, {
+      spread: 60
+    });
+
+    makeConfettiShot(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8
+    });
+
+    makeConfettiShot(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2
+    });
+
+    makeConfettiShot(0.1, {
+      spread: 120,
+      startVelocity: 45
+    });
+  }, [makeConfettiShot]);
 
   return (
     <nav className={styles.copyNavBar}>
@@ -62,6 +113,17 @@ export function CopyNavBar({ text, title }: Props) {
           height={40}
         />
       </button>
+      <ReactCanvasConfetti
+        refConfetti={getInstance}
+        style={{
+          position: "fixed",
+          pointerEvents: "none",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0
+        }}
+      />{" "}
     </nav>
   );
 }
