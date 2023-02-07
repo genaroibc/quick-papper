5a453b899bb9176736ca04ddad6379adee87d623
genaroibc <bonavitaismael@gmail.com>
2023-02-07 12:17:24 -0300

    [REFACTOR]: use @next/font to load local font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
+import NextFont from "@next/font/local";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import "./globals.css";
+
+const localFont = NextFont({
+  src: "../../public/fonts/Epilogue-VariableFont_wght.woff"
+});
 
 export default function RootLayout({
   children
@@ -8,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={localFont.className}>
       <head />
       <body>
         <Header />
